01e81ec63cbeb4c97592fba0c3db8ef7e1c5ff07
L-Nash <113747132+L-Nash@users.noreply.github.com>
2023-02-22 07:59:09 -0600

    update

---
+++ static/mapLogic.js
@@ -0,0 +1,44 @@
+//Set up Map
+var fireballMap = L.map("map", {
+    center: [0, 0],
+    zoom: 3
+  });
+ 
+// add map layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(fireballMap);
+
+// var csv= "https://raw.githubusercontent.com/L-Nash/Project-3-/main/Resources/cleaned_fireball_data.csv";
+
+var csv = "Resources/fireball_dropna.csv"
+
+
+// // // //get data
+d3.csv(csv).then(function(data) {
+layer1 =   L.geoJson(data, {
+    pointToLayer: createMarkers(data),
+    }).addTo(fireballMap); 
+
+
+//  L.marker([33.1, 173.7]).addTo(fireballMap);
+ 
+// var long = data['Longitud (dg.)'];
+var latlng = [data['Latitude (deg.)'], data['Longitud (dg.)']];
+
+function createMarkers(data, latlng) {
+        console.log(data);
+          return L.circleMarker([latlng]).bindPopup("hello");
+
+};
+
+
+});
+
+
+
+
+
+
+
+
