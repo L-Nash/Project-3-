e6113019ae692b58c734471511c6076a948f38a4
L-Nash <113747132+L-Nash@users.noreply.github.com>
2023-02-21 19:10:17 -0600

    ln

---
+++ static/js/visualsLogic.js
@@ -0,0 +1,44 @@
+// Function for updating page when year is selected
+function optionChanged(selected_year) {
+    console.log(selected_year);
+
+    // Read in fireball data with D3 library
+    const url= "https://raw.githubusercontent.com/L-Nash/Project-3-/main/Resources/fireball_data_years.csv";
+
+    d3.csv(url).then(function(data) {
+        console.log(data);
+
+
+        // Coding to create charts can go here
+
+
+
+
+
+
+
+
+
+
+
+    });
+
+
+}
+
+// Read in file
+d3.csv("https://raw.githubusercontent.com/L-Nash/Project-3-/main/Resources/fireball_data_years.csv").then(function(data) {
+    console.log(data);
+
+    var dropdownMenu = d3.select("#selDataset");
+
+    // Create elements for each year 
+    for (var i=0; i<data.length; i++) {
+        var entry = data[i];
+        dropdownMenu.append("option").text(entry['Peak Brightness Date/Time (UT)']);
+    }
+    
+    // Set default plots
+    optionChanged("2023");
+
+});
