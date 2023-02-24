0b653a65aac6ddfd7a8e4a609cf1d343885fbf49
L-Nash <113747132+L-Nash@users.noreply.github.com>
2023-02-23 21:09:48 -0600

    charts

--- static/mapLogic.js
+++ static/mapLogic.js
@@ -60,3 +60,47 @@ function createMarkers(data, latlng) {
 
 
 
+    createMap(markerLayers);
+}
+
+// Function to create map
+function createMap(markerLayers) {
+
+    // Create the base layers
+    // Street map and topographic map
+    var street = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    });
+
+    var topo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    // Create a baseMaps object
+    var baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+
+    // Create object to hold marker overlay
+    var overlayMaps = {
+        "1988 - 1999": markerLayers[0],
+        "2000 - 2009": markerLayers[1],
+        "2010 - 2019": markerLayers[2],
+        "2020 - 2023": markerLayers[3]
+    };
+    
+    // Create the map object
+    var fireballMap = L.map("map", {
+        center: [0.00, 0.00],
+        zoom: 2,
+        layers: [street, markerLayers[0]]
+    }); 
+
+    console.log(fireballMap);
+
+    L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(fireballMap); 
+
+    return fireballMap;
+}
