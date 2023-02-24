180e82f850013f93342fcd5599b26e7b2a713239
L-Nash <113747132+L-Nash@users.noreply.github.com>
2023-02-24 00:35:54 -0600

    var updates

--- static/mapLogic.js
+++ static/mapLogic.js
@@ -1,64 +1,128 @@
-//Set up Map
-var fireballMap = L.map("map", {
-    center: [0, 0],
-    zoom: 3
-  });
- 
-// add map layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(fireballMap);
+// Store URL
+const url = "https://raw.githubusercontent.com/L-Nash/Project-3-/main/Resources/fireball_sqlite.csv";
 
-// var csv= "https://raw.githubusercontent.com/L-Nash/Project-3-/main/Resources/cleaned_fireball_data.csv";
-
-var csv = "Resources/fireball_dropna.csv"
-
-
-// // //get data
-d3.csv(csv).then(function(data) {
-layer1 =   L.geoJson(data, {
-    pointToLayer: createMarkers(data),
-    }).addTo(fireballMap); 
-
-
- L.marker([33.1, 173.7]).addTo(fireballMap);
- 
-var long = data['Longitud (dg.)'];
-var lat = data['Latitude (deg.)'];
-
-function createMarkers(data, latlng) {
-        console.log(data);
-          return L.circleMarker([latlng]).bindPopup("hello");
-
-};
-
-
+// Read in fireball data with D3 library
+d3.csv(url).then(function(data){
+    console.log(data)
+    createMarkers(data);
 });
 
-// d3.csv(csv).then(function(data) {
+function createMarkers(data) {
 
-// // var long = data['Longitud (dg.)'];
-// // var lat = data['Latitude (deg.)'];
+    var markers = [];
 
-// for (var i = 0; i < data.length; i++) {
-//   layer1 =   L.geoJson(data, {
-//   L.marker(latlng, {
-//     fillOpacity: 0.75,
-//     color: "white",
-//     fillColor: "purple",
-    
-//     radius: 10
-//   }).addTo(fireballMap)};
+    // Create empty marker lists to filter by decades
+    var radEn = [];
+    var alt   = [];
+    var vel = []; 
+    var imEn = [];
+    var tenYear = [];
 
-// }
+    // Loop to get data for each record
+    for (var i=0; i<data.length; i++) {
 
-// });
+        // Store data from record
+        var fireball = data[i];
+        var latitude;
+        var longitude;
+       
+        // Pull values from columns
+        Lat = fireball['Latitude (deg.)'];
+        Lng = fireball['Longitude (deg.)'];
+
+        // Try catch to split geoetic markers
+        try {
+            
+            // Remove end value (directional marker) and store as a variable
+            latDm = Lat.slice(-1);
+            lngDm = Lng.slice(-1);
+
+            // Store just the numeric values 
+            numLat = Number(Lat.slice(0,-1));
+            numLng = Number(Lng.slice(0,-1));
+        }
+        catch (exception) {
+            // Exit if exception encountered
+            break; 
+        }
 
 
+        // Get negative value according to directional marker
+        if (lngDm == "W") {
+            longitude = numLng * -1;
+        }
+        else {longitude = numLng;}
+
+        if (latDm == "S") {
+            latitude = numLat * -1;
+        }
+        else {latitude = numLat;}
+        
+        
+
+        // Popup text variables
+        var date = (new Date(fireball['Peak Brightness Date/Time (UT)']));
+        var location = [latitude, longitude];
+        var energy = fireball['Total Radiated Energy (J)'];
+        var energyIM = fireball['Calculated Total Impact Energy (kt)'];
+        var velo = fireball['Velocity (km/s'];
+        // Retrieve just the year from the date value
+        var dYear = date.getFullYear();
+
+        // Marker details according to year
+            if (dYear >= 2013) {
+            var marker = L.circleMarker(location)
+            .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
+            + energy + "</p>");
+            tenYear.push(marker);
+            }
+
+            if (dYear >=2013) {
+            var marker = L.circleMarker(location)
+                .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
+                    + energy + "</p>");
+            alt.push(marker);
+            }
+     
+            if (dYear >= 2013 && energy >= 500000000000) {
+            var marker = L.circleMarker(location, {
+                // radius:energy/50000000000000,
+                fillColor: "red",
+                color: "red",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: energy/5000000000000
+            }).bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
+                    + energy + "</p>");
+            radEn.push(marker);
+            }
+       
+            if (dYear >= 2013 && velo >= 500) {
+            var marker = L.circleMarker(location)
+                .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
+                    + energy + "</p>");
+            vel.push(marker);
+            }
+            
+            if (dYear >= 2013 && energyIM >= .7) {
+            var marker = L.circleMarker(location)
+                .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
+                    + energy + "</p>");
+            imEn.push(marker);
+            }
 
 
+    // Create marker layers then store into array
+    var All = L. layerGroup(tenYear);
+    var Altitude = L.layerGroup(alt);
+    var Total_RE= L.layerGroup(radEn);
+    var Velocity = L.layerGroup(vel);
+    var Total_IE = L.layerGroup(imEn);
+     
 
-
+    // Store all layers into a single array
+    var markerLayers = [All, Altitude, Total_RE, Velocity, Total_IE];
+    }
 
     createMap(markerLayers);
 }
@@ -68,27 +132,29 @@ function createMap(markerLayers) {
 
     // Create the base layers
     // Street map and topographic map
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+    var street =  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+        maxZoom: 16
     });
-
+    
     var topo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
 
     // Create a baseMaps object
     var baseMaps = {
-        "Street Map": street,
+        "World Map": street,
         "Topographic Map": topo
     };
 
     // Create object to hold marker overlay
     var overlayMaps = {
-        "1988 - 1999": markerLayers[0],
-        "2000 - 2009": markerLayers[1],
-        "2010 - 2019": markerLayers[2],
-        "2020 - 2023": markerLayers[3]
+        "All Sightings 2013-2023": markerLayers[4],
+        "High Altitude": markerLayers[0],
+        "High Velocity": markerLayers[1],
+        "High Total Radiated Energy": markerLayers[2],
+        "High Total Impact Energy": markerLayers[3],
+         
     };
     
     // Create the map object
@@ -98,7 +164,7 @@ function createMap(markerLayers) {
         layers: [street, markerLayers[0]]
     }); 
 
-    console.log(fireballMap);
+//     console.log(fireballMap);
 
     L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(fireballMap); 
 
