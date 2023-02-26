6bf375a4ab869fde0042d1b275b6a1c16a3c7d46
L-Nash <113747132+L-Nash@users.noreply.github.com>
2023-02-26 04:22:24 -0600

    ln updates

--- static/mapLogic.js
+++ static/mapLogic.js
@@ -9,21 +9,26 @@ var averageAlt = 0.0;
 d3.csv(url).then(function(data){
     console.log(data)
    getAverageIE(data)
-    createMarkers(data);
-});
+   createMarkers(data)
+ });
+
+ 
+// Function to get average values of each data category
 function getAverageIE (data) {
     var Impactenergy = 0.0
     var veloc = 0.0
     var Radiateden = 0.0
     var Alt_titude  = 0.0
 
+    //loop through data to increase value of data category
     for (var i=0; i<data.length; i++) {
         Impactenergy = Impactenergy + (+data[i]['Calculated Total Impact Energy (kt)']);
         veloc = veloc + (+data[i]['Velocity (km/s)'])
         Radiateden = Radiateden + (+data[i]['Total Radiated Energy (J)'])
         Alt_titude = Alt_titude + (+data[i]['Altitude (km)'])
        
-    }   
+    }  
+    // Calculate averages 
     averageIE = Impactenergy/data.length;
     averageRE = Radiateden/data.length;
     averageVe = veloc/data.length;
@@ -31,18 +36,6 @@ function getAverageIE (data) {
     
 
 }
-
-// function setRadius (data, i, IE) {
-//     var list = data[IE].sort();
-//      var newList = list.slice(10);
-//  // compare value to new list to see if they match
-//  for (var i=0; i<newList.length; i++) {
-//      row = newlist[i];
-//  if (row[IE] = data[IE])
-//   return 10
-//   else return 5};
- 
-//  }
 
 function createMarkers(data) {
 
@@ -54,7 +47,7 @@ function createMarkers(data) {
     var vel = []; 
     var imEn = [];
     var tenYear = [];
-    console.log(radEn)
+    // console.log(radEn)
 
     // Loop to get data for each record
     for (var i=0; i<data.length; i++) {
@@ -105,85 +98,91 @@ function createMarkers(data) {
         var energyIM = fireball['Calculated Total Impact Energy (kt)'];
         var velo = fireball['Velocity (km/s)'];
         var altt = fireball['Altitude (km)'];
+        // console.log(velo)
         // Retrieve just the year from the date value
         var dYear = date.getFullYear();
+        // console.log(dYear)
+        //calculate above above average values
         var aboveAveIE = (+fireball['Calculated Total Impact Energy (kt)']) > averageIE;
         var aboveAverageRE = (+fireball['Total Radiated Energy (J)']) > averageRE;
         var aboveAverageVe = (+fireball['Velocity (km/s)']) > averageVe;
         var aboveAverageAlt = (+fireball['Altitude (km)']) > averageAlt;
 
-
-        // Marker details according to year
+        //limiting data to 2012 - 2022 so maps are easier to view
+        // Marker details to all sightings layer. Radius is larger of fireball has above average readings in all categories
             if (dYear >= 2012 && dYear <= 2022) {
-                if(aboveAveIE + aboveAverageRE + aboveAverageAlt +aboveAverageVe) 
-                    radius = 10; 
+                if(aboveAveIE && aboveAverageRE &&  aboveAverageAlt && aboveAverageVe) 
+                    radius = 10, 
+                    fillOpacity = 1;
                 else 
-                    radius = 4;
+                    radius = 4, 
+                    fillOpacity = 1;
             var marker = L.circleMarker(location, {
                 radius: radius,
-                fillColor: "green",
-                color: "green",
+                fillColor: getColor(dYear),
+                color: getColor(dYear),
                 weight: 1,
                 opacity: 1,
-                fillOpacity: .5})
-            .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
-            + energy + "</p>");
+                fillOpacity: fillOpacity})
+            .bindPopup("<p>" + date + "</p> <hr> <p>Location: " + Lat + ", " + Lng 
+            + "</p><p>Altitude): " + altt + "</p>"
+            + "</p><p>Calculated Impact: " + energyIM + "</p>"
+            + "</p><p>Total Radiated Energy (J): " + energy + "</p>"
+            + "</p><p>Velocity: " + velo + "</p>");
             tenYear.push(marker);
             }
 
-            if (dYear >= 2012 && dYear <= 2022) {
-            var marker = L.circleMarker(location, {
-                // radius:energy/50000000000000,
-                fillColor: "white",
-                color: "white",
+            // Set marker for each data category. limit datapoints to 2012-2022. 
+            if ((dYear >= 2012 && dYear <= 2022) && altt!=='') {
+             var marker = L.circleMarker(location, {
+                radius:5,
+                fillColor: "#54278f",
+                color: "#54278f",
                 weight: 1,
                 opacity: 1,
-                fillOpacity: altt/10})
-                .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
-                    + energy + "</p>");
-            alt.push(marker);
+                fillOpacity: altt/60 })
+                .bindPopup("<p><h4>Altitude): " + altt + "</h4></p> <hr> <p>Location: " + Lat + ", " + Lng );
+                alt.push(marker);
             }
-     
-            if ((dYear >= 2012 && dYear <= 2022)) {
+            
+            //radiated energy
+            if ((dYear >= 2012 && dYear <= 2022) && energy!=='') {
             var marker = L.circleMarker(location, {
-                // radius:energy/50000000000000,
-                fillColor: "red",
-                color: "red",
+                radius:5,
+                fillColor: "#253494",
+                color: "#2c7fb8",
                 weight: 1,
                 opacity: 1,
                 fillOpacity: energy/500000000000})
-            .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
-                    + energy + "</p>");
-            radEn.push(marker);
+                .bindPopup("<p><h4>Total Radiated Energy (J): " + energy + "</h4></p> <hr> <p>Location: " + Lat + ", " + Lng );
+                radEn.push(marker);
             }
-       
-            if ((dYear >= 2012 && dYear <= 2022)) {
+            //Velocity
+            if ((dYear >= 2012 && dYear <= 2022) && velo!=='') {
+                 var marker = L.circleMarker(location, {
+                    radius:5,
+                    fillColor: "#a50f15",
+                    color: "#fb6a4a",
+                    weight: 1,
+                    opacity: 1, 
+                    fillOpacity: velo/35})
+                    .bindPopup("<p><h4>Velocity: " + velo + "</h4></p> <hr> <p>Location: " + Lat + ", " + Lng );
+                    vel.push(marker);
+            }
+            //Impact Energy
+            if ((dYear >= 2012 && dYear <= 2022) && energyIM!=='') {
             var marker = L.circleMarker(location, {
-                // radius:energy/50000000000000,
-                fillColor: "yellow",
-                color: "yellow",
+                radius:5,
+                fillColor: "#006d2c",
+                color: "#006d2c",
                 weight: 1,
                 opacity: 1,
-                fillOpacity: velo})
-                .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
-                    + energy + "</p>");
-            vel.push(marker);
-            }
-            
-
-            if ((dYear >= 2012 && dYear <= 2022) && energyIM >= .2) {
-            var marker = L.circleMarker(location, {
-            fillColor: "purple",
-            color: "purple",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: energyIM})
-                .bindPopup("<p>Date: " + date + "</p><p>Location: " + Lat + ", " + Lng + "</p><p>Total Radiated Energy (J): " 
-                    + energy + "</p>");
-            imEn.push(marker);
+                fillOpacity: energyIM})
+                .bindPopup("<p><h4>Calculated Impact: "+ energyIM + "</h4></p> <hr> <p>Location: " + Lat + ", " + Lng );
+                imEn.push(marker);
             }
 
-       
+    
     // Create marker layers then store into array
     var All = L. layerGroup(tenYear);
     var Altitude = L.layerGroup(alt);
@@ -194,9 +193,11 @@ function createMarkers(data) {
 
     // Store all layers into a single array
     var markerLayers = [All, Altitude, Total_RE, Velocity, Total_IE];
-    }
 
+  }
+  
     createMap(markerLayers);
+    
 }
 
 // Function to create map
@@ -204,12 +205,15 @@ function createMap(markerLayers) {
 
     // Create the base layers
     // Street map and topographic map
-    var street =  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+    var street =  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
     
-    var topo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    
+    var topo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service',
+        maxZoom: 8
     });
 
     // Create a baseMaps object
@@ -234,10 +238,28 @@ function createMap(markerLayers) {
         layers: [street, markerLayers[0]]
     }); 
 
+   
 //     console.log(fireballMap);
 
     L.control.layers(baseMaps, overlayMaps, {collapsed: false}).addTo(fireballMap); 
+    
 
     return fireballMap;
 
+
+    
+
 }
+
+
+
+
+
+function getColor(yr){
+if (yr >= 2012 && yr < 2017)
+    return "#c51b8a"; 
+else 
+    return "#7a0177";
+
+}
+
